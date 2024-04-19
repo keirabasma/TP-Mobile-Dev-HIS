3fc1e2015151817ac780bef318728d2af4524598
ka0094 <abdelkarimhaddad10@gmail.com>
2024-04-19 18:32:52 +0100

    Moyenne

---
+++ moyenne/app/src/main/java/com/example/moyenne/MainActivity2.java
@@ -0,0 +1,18 @@
+package com.example.moyenne;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        TextView resultTextView = findViewById(R.id.resultTextView);
+        String message = getIntent().getStringExtra("message");
+        resultTextView.setText(message);
+    }
+}
