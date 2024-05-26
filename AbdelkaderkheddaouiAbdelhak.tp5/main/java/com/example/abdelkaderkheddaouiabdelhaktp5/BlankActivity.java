d2c44cd53db55fc64fad21e38f2ded4282203693
Abdelhakk1 <120207448+Abdelhakk1@users.noreply.github.com>
2024-05-26 17:38:44 +0100

    Add files via upload

---
+++ AbdelkaderkheddaouiAbdelhak.tp5/main/java/com/example/abdelkaderkheddaouiabdelhaktp5/BlankActivity.java
@@ -0,0 +1,13 @@
+package com.example.abdelkaderkheddaouiabdelhaktp5;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BlankActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blank);
+    }
+}
