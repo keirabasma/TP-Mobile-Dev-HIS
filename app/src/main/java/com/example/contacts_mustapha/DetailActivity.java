032868c2d96d5a240f3418eec7ba91132aaa34d6
mustbchn <162446076+mustbchn@users.noreply.github.com>
2024-05-27 00:09:00 +0100

    new tp 4-5

---
+++ app/src/main/java/com/example/contacts_mustapha/DetailActivity.java
@@ -0,0 +1,50 @@
+package com.example.contacts_mustapha;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.details);
+        String name = getIntent().getStringExtra("name");
+        String lastname = getIntent().getStringExtra("lastname");
+
+        TextView entredNum = findViewById(R.id.textView3);
+        entredNum.setText(name);
+
+        TextView entredNum2 = findViewById(R.id.textView2);
+        entredNum2.setText(lastname);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.detailss), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+
+
+
+    }
--- app/src/main/java/com/example/contacts_mustapha/MainActivity.java
+++ app/src/main/java/com/example/contacts_mustapha/MainActivity.java
@@ -1,24 +1,105 @@
 package com.example.contacts_mustapha;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.contacts_mustapha.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ArrayList<Contact> mylist = new ArrayList<>();
+        mylist.add(new Contact("Mustapha", "Bouchene", "0566328465"));
+        mylist.add(new Contact("Omar", "Boutan", "0654213985"));
+        mylist.add(new Contact("Fatima", "Onio", "0548653215"));
+        mylist.add(new Contact("Kamila", "Latrech", "0548653215"));
+
+        ListView lv = findViewById(R.id.listep);
+        ContactAdapter myAdapter = new ContactAdapter(this, R.layout.contact_lay, mylist);
+        lv.setAdapter(myAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
+
+                Contact selectedContact = mylist.get(position);
+
+
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra("name", selectedContact.name);
+                intent.putExtra("lastname", selectedContact.lastname);
+                //intent.putExtra("name", "000");
+               // intent.putExtra("lastname", "222");
+              //  intent.putExtra("phone", selectedContact.phone);
+
+                startActivity(intent);
+            }
         });
+    }
+
+    public class Contact {
+        public String name;
+        public String lastname;
+        public String phone;
+
+        public Contact(String name, String lastname, String phone){
+            this.name = name;
+            this.lastname = lastname;
+            this.phone = phone;
+        }
+    }
+
+    public class ContactAdapter extends ArrayAdapter<Contact> {
+        ArrayList<Contact> contactlist;
+        LayoutInflater inflat;
+        int Rc;
+
+        public ContactAdapter(MainActivity c, int resource, ArrayList<Contact> L) {
+            super(c, resource, L);
+            inflat = LayoutInflater.from(c);
+            Rc = resource;
+            contactlist = L;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            if (v == null)
+                v = inflat.inflate(Rc, null);
+            TextView tvName = v.findViewById(R.id.name);
+            TextView tvPrenom = v.findViewById(R.id.lastname);
+            tvName.setText(contactlist.get(position).name);
+            tvPrenom.setText(contactlist.get(position).lastname);
+            return v;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
