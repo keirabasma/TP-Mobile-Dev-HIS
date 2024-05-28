33a223018735c50ca4c06375e751cd4566b3d30e
MESines <154763793+MESines@users.noreply.github.com>
2024-05-28 23:52:15 +0100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,51 +1,113 @@
-package com.example.tp1calc;
+package com.example.clacul_tp1;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-TextView resultTV,solutionTV;
-MaterialButton C,AC,buttonp,buttondiv;
-    MaterialButton button7,button8,button9,buttonX;
-    MaterialButton button4,button5,button6,buttonmin;
-    MaterialButton button1,button2,button3,buttonplus;
-    MaterialButton button00,button0,buttonvg,buttonegal;
+    TextView resultTV, solutionTV;
+    String operand = "";
+    double num1 = 0, num2 = 0;
+    char operator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resultTV=findViewById(R.id.result_tv);
 
-        solutionTV=findViewById(R.id.solution_tv);
-        assignId(AC,R.id.AC);
-        assignId(C,R.id.c);
-        assignId(buttonp,R.id.p);
-        assignId(buttondiv,R.id.div);
-        assignId(button7,R.id.button7);assignId(button8,R.id.button8);assignId(button9,R.id.button9);assignId(buttonX,R.id.buttonX);
-        assignId(button4,R.id.button4);assignId(button5,R.id.button5);assignId(button6,R.id.button6);assignId(buttonmin,R.id.buttonmin);
-        assignId(button1,R.id.button1);assignId(button2,R.id.button2);assignId(button3,R.id.button3);assignId(buttonplus,R.id.buttonplus);
-        assignId(button00,R.id.button00);assignId(button0,R.id.button0);assignId(buttonvg,R.id.buttonver);assignId(buttonegal,R.id.buttonegl);
-    }
-    void assignId(MaterialButton btn, int id){
-        btn=findViewById(id);
-        btn.setOnClickListener(this);
+        resultTV = findViewById(R.id.result_tv);
+        solutionTV = findViewById(R.id.solution_tv);
+
+        // Assign onClick listener to buttons
+        findViewById(R.id.AC).setOnClickListener(this);
+        findViewById(R.id.c).setOnClickListener(this);
+        findViewById(R.id.p).setOnClickListener(this);
+        findViewById(R.id.div).setOnClickListener(this);
+        findViewById(R.id.button7).setOnClickListener(this);
+        findViewById(R.id.button8).setOnClickListener(this);
+        findViewById(R.id.button9).setOnClickListener(this);
+        findViewById(R.id.buttonX).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
+        findViewById(R.id.button5).setOnClickListener(this);
+        findViewById(R.id.button6).setOnClickListener(this);
+        findViewById(R.id.buttonmin).setOnClickListener(this);
+        findViewById(R.id.button1).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.buttonplus).setOnClickListener(this);
+        findViewById(R.id.button00).setOnClickListener(this);
+        findViewById(R.id.button0).setOnClickListener(this);
+        findViewById(R.id.buttonver).setOnClickListener(this);
+        findViewById(R.id.buttonegl).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        MaterialButton button=(MaterialButton) v;
-        String Buttontext=button.getText().toString();
-        solutionTV.setText(Buttontext);
+        String buttonText = ((MaterialButton) v).getText().toString();
+        switch (buttonText) {
+            case "AC":
+                clearAll();
+                break;
+            case "C":
+                clear();
+                break;
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                operator = buttonText.charAt(0);
+                num1 = Double.parseDouble(solutionTV.getText().toString());
+                clear();
+                break;
+            case "=":
+                num2 = Double.parseDouble(solutionTV.getText().toString());
+                calculate();
+                break;
+            default:
+                operand += buttonText;
+                solutionTV.setText(operand);
+                break;
+        }
+    }
 
+    private void clearAll() {
+        solutionTV.setText("0");
+        resultTV.setText("0");
+        operand = "";
+        num1 = 0;
+        num2 = 0;
+        operator = ' ';
+    }
+
+    private void clear() {
+        operand = "";
+        solutionTV.setText("0");
+    }
+
+    private void calculate() {
+        double result = 0;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else
+                    result = Double.NaN; // Indicate division by zero
+                break;
+        }
+        resultTV.setText(String.valueOf(result));
+        operand = "";
     }
 }
