8b85665bc9519dcf86108137d78b29c1217b0bd9
wafahr <124716179+wafahr@users.noreply.github.com>
2024-05-28 23:55:20 +0100

    Add files via upload

---
+++ NewActivity.java
@@ -0,0 +1,15 @@
+package com.example.login;
+
+
+    import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+    public class NewActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.newactivity);
+        }
+    }
+
+
