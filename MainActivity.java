8d55de3ca345a56b656088759acefe1445a10834
wafahr <124716179+wafahr@users.noreply.github.com>
2024-05-28 23:58:54 +0100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,95 +1,47 @@
-package com.example.calculator;
+package com.example.liste;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText firstNumber;
-    private EditText secondNumber;
-    private TextView resultText;
+    private ArrayList<Contact> contactList;
+    private ContactAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        firstNumber = findViewById(R.id.Number1);
-        secondNumber = findViewById(R.id.Number2);
-        resultText = findViewById(R.id.result);
+        contactList = new ArrayList<>();
+        ListView listView = findViewById(R.id.contactListView);
 
-        Button addButton = findViewById(R.id.addButton);
-        Button subtractButton = findViewById(R.id.subtractButton);
-        Button multiplyButton = findViewById(R.id.multipButton);
-        Button divideButton = findViewById(R.id.divideButton);
+        contactList.add(new Contact("nom1", "prenom1", "123456"));
+        contactList.add(new Contact("nom2", "prenom2", "123456"));
+        contactList.add(new Contact("nom3", "prenom3", "123456"));
+        contactList.add(new Contact("nom4", "prenom4", "123456"));
+        contactList.add(new Contact("nom5", "prenom5", "123456"));
 
-        addButton.setOnClickListener(new View.OnClickListener() {
+        adapter = new ContactAdapter(this, R.layout.contact, contactList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                calculate('+');
-            }
-        });
-
-        subtractButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculate('-');
-            }
-        });
-
-        multiplyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculate('*');
-            }
-        });
-
-        divideButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calculate('/');
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Contact selectedContact = contactList.get(position);
+                Intent intent = new Intent(MainActivity.this, detailactivity.class);
+                intent.putExtra("contact", selectedContact);
+                startActivity(intent);
             }
         });
     }
 
-    private void calculate(char operator) {
-        String num1Str = firstNumber.getText().toString();
-        String num2Str = secondNumber.getText().toString();
-
-        if (num1Str.isEmpty() || num2Str.isEmpty()) {
-            Toast.makeText(this, "Please enter numbers", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        double num1 = Double.parseDouble(num1Str);
-        double num2 = Double.parseDouble(num2Str);
-        double result = 0;
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    Toast.makeText(this, "Cannot divide by zero", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                break;
-        }
-
-        resultText.setText(String.valueOf(result));
-    }
 }
