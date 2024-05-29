3deb2532face61449be09ef75a8cafc54ddaa5b5
HamiciYoucef <hamici.youghiles@gmail.com>
2024-05-29 13:16:59 +0100

    Moyenne

---
+++ Moyenne/app/src/main/java/com/example/tp2/SuccedActivity.java
@@ -0,0 +1,20 @@
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tp2.R;
+
+public class SuccedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.secondactivity);
+
+        float moyenne = getIntent().getFloatExtra("moyenne", 0);
+
+        TextView textViewMoyenne = findViewById(R.id.textViewMoyenne);
+        textViewMoyenne.setText("Votre moyenne : " + moyenne);
+    }
+}
---
+++ Moyenne/app/src/main/java/com/example/tp2/sucssedActivity.java
@@ -0,0 +1,4 @@
+package com.example.tp2;
+
+public interface sucssedActivity {
+}
